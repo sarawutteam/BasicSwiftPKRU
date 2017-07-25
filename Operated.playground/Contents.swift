a55b23e50e6afe17f263dd44bda3fd1da00c3dba
Com06 <com06@Com06s-iMac.local>
2017-07-25 14:11:04 +0700

    Oparated

---
+++ Operated.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var strNumber:String = "5" //ไม่ใช่ตัวเลข แต่เป็นตัวอักษร 5 จึงไม่ใช้ int 
+var intNumber: Int = 20
+
+let intNewNumber = Int(strNumber) // Change String ==> int การเปลี่ยนจาอักษรำแเป้นตัวเลข
+
+
+
+let intAnswer = intNewNumber! + intNumber
+
+let intAnswer2 = intAnswer/intNewNumber!
+
